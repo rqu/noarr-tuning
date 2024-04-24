49335a8e85780b4542f60beac10354b017133d6d
Lukáš Rozsypal <33589083+rqu@users.noreply.github.com>
2024-04-24 07:45:25 +0200

    Modify opentuner_subproc_test to actually test opentuner_subproc

--- tests/opentuner_subproc_test/test_direct.cpp
+++ tests/opentuner_subproc_test/test_direct.cpp
@@ -1,10 +1,15 @@
-#include <noarr/tuning/formatters/opentuner_formatter.hpp>
+#include <noarr/tuning/formatters/opentuner_subproc.hpp>
 
-static const char *GCC_FLAGS = "-O2 -g -std=c++20 -o tmp.bin -I../../../include -I../build/_deps/noarr-src/include " __FILE__;
-#define SPECIFIC_TUNING_BEGIN(...) NOARR_TUNE_BEGIN(noarr::tuning::opentuner_formatter( \
-		std::cout, \
-		noarr::tuning::direct_compile_command_builder("g++", GCC_FLAGS), \
-		noarr::tuning::direct_run_command_builder("./tmp.bin", "../matrices 512"), \
-		"return Result(time=int(run_result['stderr'].split()[2]))"))
+static constexpr auto main_cpp_file = __FILE__;
+
+static constexpr auto measure_command = [](auto &proc, auto Result, auto run_result) {
+	return Result(snaketongs::kw("time")=proc.int_(run_result["stderr"].call("split")[2]));
+};
+
+#define SPECIFIC_TUNING_BEGIN(...) NOARR_TUNE_BEGIN(noarr::tuning::opentuner_subproc( \
+		{"g++", "-O2", "-g", "-std=c++20", "-o" "tmp.bin", "-I" "../../../include", "-I" "../build/_deps/noarr-src/include", "-I" "../build/_deps/snaketongs-src/include", main_cpp_file}, \
+		{"./tmp.bin", "../matrices", "512"}, \
+		{"--test-limit=20", "--no-dups"}, \
+		measure_command))
 
 #include "../program.hpp"
